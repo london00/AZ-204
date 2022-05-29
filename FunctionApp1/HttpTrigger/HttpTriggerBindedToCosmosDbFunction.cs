08c900529ca601f7e759365a06e7186fc7127cb0
Geiser Aragon <Geiser_Aragon@epam.com>
2022-05-29 15:14:06 -0500

    Add a cosmos output binding to HttpTriggerBindedToCosmosDbFunction

--- FunctionApp1/HttpTrigger/HttpTriggerBindedToCosmosDbFunction.cs
+++ FunctionApp1/HttpTrigger/HttpTriggerBindedToCosmosDbFunction.cs
@@ -17,22 +17,26 @@ namespace FunctionApp.HttpTrigger
     public static class HttpTriggerBindedToCosmosDbFunction
     {
         [FunctionName("HttpTriggerBindedToCosmosDbFunction")]
-        public static async Task<IActionResult> Run(
+        public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "people/{id:int}")] HttpRequest req,
-            [CosmosDB("School", "People", ConnectionStringSetting = "AzureCosmosDb")] IEnumerable<People> client,
+            [CosmosDB("School", "People", ConnectionStringSetting = "AzureCosmosDb")] IEnumerable<PersonAggregate> peopleClient,
+            [CosmosDB("School", "Logs", ConnectionStringSetting = "AzureCosmosDb")] out LogAggregate logAggregate,
             [Bind("id")] int id,
             ILogger log)
+        
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            People person = GetPerson(client, id);
+            PersonAggregate person = GetPerson(peopleClient, id);
 
             string responseMessage = person is null ? "Person was not find" : $"Person with name {person.Name} was found. Id: {person.Id}";
+
+            logAggregate = new LogAggregate { Id = Guid.NewGuid().ToString(), Message = responseMessage };
 
             return new OkObjectResult(responseMessage);
         }
 
-        private static People GetPerson(IEnumerable<People> client, int id)
+        private static PersonAggregate GetPerson(IEnumerable<PersonAggregate> client, int id)
         {
             var person = client.FirstOrDefault(x => x.Id == id.ToString());
 
@@ -40,9 +44,15 @@ namespace FunctionApp.HttpTrigger
         }
     }
 
-    public class People
+    public class PersonAggregate
     {
         public string Id { get; set; }
         public string Name { get; set; }
+    }
+
+    public class LogAggregate 
+    {
+        public string Id { get; set; }
+        public string Message { get; set; }
     }
 }
