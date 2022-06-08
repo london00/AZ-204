ad3f74f7a6f8a725e5589032a22d26ed3c49076f
Geiser Aragon <Geiser_Aragon@epam.com>
2022-06-08 01:09:15 -0500

    Add a GitHubWebHubHelper to validate the key

--- FunctionApp1/HttpTrigger/GithubWebhookFunction.cs
+++ FunctionApp1/HttpTrigger/GithubWebhookFunction.cs
@@ -1,14 +1,13 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FunctionApp.HttpTrigger
 {
@@ -20,8 +19,8 @@ namespace FunctionApp.HttpTrigger
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a GitHub request.");
-            
-            await GitHubWebHubHelper.CompareSignatureAsync(req);
+
+            await GitHubWebHubHelper.CompareSignatureAsync(req, log);
 
             log.LogInformation("x-github-event: " + req.Headers["x-github-event"].ToString());
 
@@ -38,7 +37,7 @@ namespace FunctionApp.HttpTrigger
     {
         private const string MY_PRIVATE_KEY = "mykey";
 
-        public static async Task CompareSignatureAsync(HttpRequest req)
+        public static async Task CompareSignatureAsync(HttpRequest req, ILogger log)
         {
             ASCIIEncoding encoding = new ASCIIEncoding();
 
@@ -49,13 +48,30 @@ namespace FunctionApp.HttpTrigger
             var gitHubSignature = req.Headers["x-hub-signature"].ToString();
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            
+
             var shaSignature = hmacsha1.ComputeHash(encoding.GetBytes(requestBody));
 
-            if (string.Equals(gitHubSignature, shaSignature.ToString(), StringComparison.OrdinalIgnoreCase))
+            if (string.Equals(gitHubSignature, "sha1=" + ToHexString(shaSignature), StringComparison.OrdinalIgnoreCase))
             {
-                throw new CryptographicUnexpectedOperationException("GitHub WebHook ket is not valid.");
+                log.LogInformation("GitHub WebHook key: Valid");
             }
+            else
+            {
+                log.LogInformation("GitHub WebHook key: Invalid");
+
+                throw new CryptographicUnexpectedOperationException("GitHub WebHook key is not valid.");
+            }
+        }
+
+        private static string ToHexString(byte[] bytes)
+        {
+            var builder = new StringBuilder(bytes.Length * 2);
+            foreach (byte b in bytes)
+            {
+                builder.AppendFormat("{0:x2}", b);
+            }
+
+            return builder.ToString();
         }
     }
 }
