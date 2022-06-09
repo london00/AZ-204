097523389685361c620f907e2273e6354c694986
Geiser Aragon <Geiser_Aragon@epam.com>
2022-06-09 11:06:21 -0500

    Secure azure functions to use app secrets.

---
+++ FunctionApp1/Startup.cs
@@ -0,0 +1,33 @@
+﻿using FunctionApp;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace FunctionApp
+{
+    // inherit FunctionsStartup
+    public class Startup : FunctionsStartup
+    {
+        // override configure method
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+               .SetBasePath(Environment.CurrentDirectory)
+               .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+               .AddEnvironmentVariables();
+
+
+            // register your other services
+            if (Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") == "Development")
+            {
+                config.AddUserSecrets<Startup>(true);
+            }
+
+            var settings = config.Build();
+
+            builder.Services.AddSingleton<IConfiguration>(settings);
+        }
+    }
+}
